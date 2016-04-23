c718025ca96d376087a83d0a6ead6c661d176432
Patrick Martin <patrick145@msn.com>
2016-04-23 02:43:19 -0400

    Changed so that there is a central node for focus. Navigation was causing orphaning.

--- src/charts/d3plus/Network.js
+++ src/charts/d3plus/Network.js
@@ -1,104 +1,104 @@
 var network = function (userConfig) {
-    var chart;
+  var chart;
 
-    var defaults =
-    {
-        // The parent container of this chart.
-        'parent': '#RingNetwork',
-        // Set these when you need to CSS style components independently.
-        'id': 'RingNetwork',
-        'class': 'RingNetwork',
-        'resizable': true,
-        // Our data...
-        'csv': {
-            // Give folks without data something to look at anyhow.
-            'header': ["NAME", "GENDER", "VEHICLE"],
-            'data': [
-                ["JIM", "M", "CAR"],
-                ["JOE", "M", "CAR"],
-                ["PAT", "M", "TRUCK"],
-                ["SALLY", "F", "TRUCK"]
-            ]
-        },
-        'connect': 'first',
-        'type' : 'rings',
-        //'connect' : 'all',
-        'width': "100%",
-        'height': "100%",
-        'transform': "translate(0 0)",
-    };
+  var defaults =
+  {
+    // The parent container of this chart.
+    'parent': '#RingNetwork',
+    // Set these when you need to CSS style components independently.
+    'id': 'RingNetwork',
+    'class': 'RingNetwork',
+    'resizable': true,
+    // Our data...
+    'csv': {
+      // Give folks without data something to look at anyhow.
+      'header': ["NAME", "GENDER", "VEHICLE"],
+      'data': [
+        ["JIM", "M", "CAR"],
+        ["JOE", "M", "CAR"],
+        ["PAT", "M", "TRUCK"],
+        ["SALLY", "F", "TRUCK"]
+      ]
+    },
+    'connect': 'last',
+    'type': 'rings',
+    //'connect' : 'all',
+    'width': "100%",
+    'height': "100%",
+    'transform': "translate(0 0)",
+  };
 
-    var chart = new dex.component(userConfig, defaults);
+  var chart = new dex.component(userConfig, defaults);
 
-    chart.render = function render() {
-        window.onresize = this.resize;
-        chart.resize();
-    };
+  chart.render = function render() {
+    window.onresize = this.resize;
+    chart.resize();
+  };
 
-    chart.resize = function resize() {
-        if (chart.config.resizable) {
-            var width = d3.select(chart.config.parent).property("clientWidth");
-            var height = d3.select(chart.config.parent).property("clientHeight");
-            dex.console.log(chart.config.id + ": resize(" + width + "," + height + ")");
-            chart.attr("width", width).attr("height", height).update();
+  chart.resize = function resize() {
+    if (chart.config.resizable) {
+      var width = d3.select(chart.config.parent).property("clientWidth");
+      var height = d3.select(chart.config.parent).property("clientHeight");
+      dex.console.log(chart.config.id + ": resize(" + width + "," + height + ")");
+      chart.attr("width", width).attr("height", height).update();
+    }
+    else {
+      chart.update();
+    }
+  };
+
+  chart.update = function () {
+    var chart = this;
+    var config = chart.config;
+    var csv = config.csv;
+
+    var connections = [];
+
+    var rootMap = {};
+    var cmap = {};
+
+    // Connect everything in the row to the first column.
+    //if (config.connect == 'first') {
+    // TODO: Support other connection models here.
+    for (var ri = 0; ri < csv.data.length; ri++) {
+      if (_.isUndefined(rootMap[csv.data[ri][0]])) {
+        connections.push({'source': csv.header[0], 'target': csv.data[ri][0]});
+        rootMap[csv.data[ri][0]] = 1;
+      }
+
+      for (var ci = 1; ci < csv.header.length; ci++) {
+        var src = csv.data[ri][0];
+        var dest = csv.data[ri][ci];
+
+        if (_.isUndefined(cmap[src + " -> " + dest])) {
+          connections.push({'source': src, 'target': dest});
+          cmap[src + " -> " + dest] = 1;
         }
-        else {
-            chart.update();
-        }
-    };
+      }
+    }
 
-    chart.update = function () {
-        var chart = this;
-        var config = chart.config;
-        var csv = config.csv;
+    //dex.console.log("Connections", connections);
 
-        var connections = [];
+    // instantiate d3plus
+    var viz = d3plus.viz()
+      .container(config.parent)
+      .type(config.type)
+      .edges(connections)
+      .focus(csv.header[0]);
 
-        // Connect everything in the row to the first column.
-        if (config.connect == 'first') {
-            for (var ri = 0; ri < csv.data.length; ri++) {
-                for (var ci = 1; ci < csv.header.length; ci++) {
-                    connections.push({'source': csv.data[ri][0], 'target': csv.data[ri][ci]});
-                }
-            }
-        }
-        // Connect everything in the row to everything else in the row.
-        else {
-            for (var ri = 0; ri < csv.data.length; ri++) {
-                for (var ci = 1; ci < csv.header.length; ci++) {
-                    connections.push({'source': csv.data[ri][ci - 1], 'target': csv.data[ri][ci]});
-                }
-                connections.push({'source': csv.data[ri][csv.header.length - 1], 'target': csv.data[ri][0]});
-            }
-        }
+    if (config.edges) {
+      viz.edges(config.edges);
+    }
 
-        //dex.console.log("Connections", connections);
+    viz.draw();
+  };
 
-        // instantiate d3plus
-        var viz = d3plus.viz()
-            .container(config.parent)
-            .type(config.type)
-            .edges(connections);
+  $(document).ready(function () {
+    // Make the entire chart draggable.
+    //$(chart.config.parent).draggable();
+  });
 
-        if (config.edges)
-        {
-            viz.edges(config.edges);
-        }
-
-        if (config.focus)
-        {
-            viz.focus(config.focus);
-        }
-
-        viz.draw();             // finally, draw the visualization!
-    };
-
-    $(document).ready(function () {
-        // Make the entire chart draggable.
-        //$(chart.config.parent).draggable();
-    });
-
-    return chart;
+  return chart;
 };
 
 module.exports = network;
