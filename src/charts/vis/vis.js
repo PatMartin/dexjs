706fa665ea69ef5b222c0dfaf8906b084c31707e
Patrick Martin <patrick145@msn.com>
2016-12-28 23:46:15 -0500

    Net vis.js Network visual.

---
+++ src/charts/vis/vis.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * This module provides routines for dealing with arrays.
+ *
+ * @module dex/charts/vis
+ * @name vis
+ * @memberOf dex.charts
+ *
+ */
+var vis = {};
+
+vis.Network = require("./Network");
+
+module.exports = vis;
