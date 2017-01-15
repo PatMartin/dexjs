7927ff81bce3458033a68727260356f6f0e4838e
Patrick Martin <patrick145@msn.com>
2017-01-15 10:26:33 -0500

    Rebuild

--- dist/dex.js
+++ dist/dex.js
@@ -1413,6 +1413,8 @@ var chord = function (userConfig) {
         };
       });
     }
+    // Allow method chaining
+    return chart;
   };
 
   $(document).ready(function () {
@@ -1429,7 +1431,7 @@ var clusteredforce = function (userConfig) {
   d3 = dex.charts.d3.d3v3;
   var defaults =
   {
-    'parent': null,
+    'parent': '#ClusteredForceParent',
     'id': "ClusteredForce",
     'class': "ClusteredForce",
     'height': "100%",
@@ -4690,8 +4692,7 @@ var parallelcoordinates = function (userConfig) {
       'opacity': .8,
       'color': "steelblue",
       'stroke': dex.config.stroke({'color': "black", 'width': 1})
-    },
-    'ui.config': {}
+    }
   };
 
   chart = new dex.component(userConfig, defaults);
@@ -5097,101 +5098,169 @@ var radarchart = function (userConfig) {
   d3 = dex.charts.d3.d3v3;
   var chart;
 
-  var defaults =
-    {
-      // The parent container of this chart.
-      'parent': '#RadarChart',
-      // Set these when you need to CSS style components independently.
-      'id': 'RadarChart',
-      'class': 'RadarChart',
-      'resizable': true,
-      // Our data...
-      'csv': {
-        // Give folks without data something to look at anyhow.
-        'header': ["Salesman", "Q1", "Q2", "Q3", "Q4"],
-        'data': [
-          ["Bob", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Sue", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Joe", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Peg", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Pat", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Jim", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Tim", Math.random(), Math.random(), Math.random(), Math.random()],
-          ["Sam", Math.random(), Math.random(), Math.random(), Math.random()]
-        ]
-      },
-      'width': "100%",
-      'height': "100%",
-      'transform': "",
-      'margin': {top: 20, right: 20, bottom: 20, left: 20},
-      'wrapWidth': 60,
-      'levels': 5,
-      'maxValue': 0,
-      'labelFactor': 1.25,
-      'dotRadius': 4,
-      'opacityCircles': 0.1,
-      'strokeWidth': 2,
-      'roundStrokes': false,
-      'color': d3.scale.category10(),
-      'title': dex.config.text(),
-      'label': dex.config.text(),
-      'opacityArea': 0.35,
-      'radar.circle': dex.config.circle({
-        'opacity': 0.1,
-        'fill': {
-          'fillColor': '#CDCDCD',
-          'fillOpacity': .2
-        },
-        'stroke': {
-          'width': 1,
-          'color': 'black',
-          'opacity': .1,
-          'dasharray': "0"
-        },
-        'events': {
-          'mouseover': function () {
-            d3.select(this)
-              .style("stroke", 'red')
-              .style("stroke-width", 2);
-          },
-          'mouseout': function () {
-            d3.select(this)
-              .style("stroke", chart.config.radar.circle.stroke.color)
-              .style("stroke-width", chart.config.radar.circle.stroke.width);
-          }
+  var defaults = {
+    // The parent container of this chart.
+    'parent': '#RadarChart',
+    // Set these when you need to CSS style components independently.
+    'id': 'RadarChart',
+    'class': 'RadarChart',
+    'resizable': true,
+    // Our data...
+    'csv': {
+      // Give folks without data something to look at anyhow.
+      'header': ["Salesman", "Q1", "Q2", "Q3", "Q4"],
+      'data': [
+        ["Bob", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Sue", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Joe", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Peg", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Pat", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Jim", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Tim", Math.random(), Math.random(), Math.random(), Math.random()],
+        ["Sam", Math.random(), Math.random(), Math.random(), Math.random()]
+      ]
+    },
+    'width': "100%",
+    'height': "100%",
+    'transform': "",
+    'margin': {
+      top: 70,
+      right: 50,
+      bottom: 70,
+      left: 50
+    },
+    'format': d3.format(','),
+    'wrapWidth': 60,
+    'levels': 5,
+    'maxValue': 0,
+    'labelFactor': 1,
+    'dotRadius': 4,
+    'opacityCircles': 0.1,
+    'strokeWidth': 2,
+    'roundStrokes': false,
+    'color': d3.scale.category10(),
+    'title': dex.config.text(),
+    'label': dex.config.text({
+      'fill.fillColor': 'green',
+      'font.size': '16px',
+      'anchor': function (d, i) {
+        var degreesPerSection = 360 / (chart.attr('csv').header.length - 1);
+        dex.console.log("Degrees Per Section", degreesPerSection, degreesPerSection * (i-1));
+        if (degreesPerSection * i < 10) {
+          return "middle";
         }
+        else if (degreesPerSection * i < 175) {
+          return "start";
+        }
+        else if (degreesPerSection * i < 185) {
+          return "middle";
+        }
+        dex.console.log("D", d, i);
+        return "end";
+      },
+      'dy' : function(d, i) {
+        var degreesPerSection = 360 / (chart.attr('csv').header.length - 1);
+        if (degreesPerSection * i < 10) {
+          return '-1em';
+        }
+        else if (degreesPerSection * i < 175) {
+          return '.3em';
+        }
+        else if (degreesPerSection * i < 185) {
+          return '1em';
+        }
+        return '.3em';
+      },
+      'dx' : function(d, i) {
+        var degreesPerSection = 360 / (chart.attr('csv').header.length - 1);
+        if (degreesPerSection * i < 10) {
+          return '0';
+        }
+        else if (degreesPerSection * i < 175) {
+          return '1em';
+        }
+        else if (degreesPerSection * i < 185) {
+          return '0';
+        }
+        return '-.3em'
+      }
+      //'dy': '.2em',
+      //'dx': '.2em'
+    }),
+    'opacityArea': 0.25,
+    // The spoke axis.
+    'spoke': {
+      'label': dex.config.text({
+        'x': 4,
+        'y': function (d) {
+          var height = chart.attr('height');
+          var width = chart.attr('width');
+          var margin = chart.attr('margin');
+
+          var radius = Math.min(height - margin.top - margin.bottom,
+              width - margin.left - margin.right) / 2;
+          return -d * radius / chart.config.levels
+        },
+        'dy': '.4em',
+        'font.size': '16px',
+        'fill.fillColor': 'red'
+      }),
+      'line': dex.config.line({
+        'stroke.width': '1px',
+        'stroke.color': 'blue',
+        'stroke.dasharray': '5 5'
       })
-    };
+    },
+    'radar.circle': dex.config.circle({
+      'opacity': 0.1,
+      'fill': {
+        'fillColor': '#CDCDCD',
+        'fillOpacity': .15
+      },
+      'stroke': {
+        'width': 1,
+        'color': 'black',
+        'opacity': .1,
+        'dasharray': "0"
+      },
+      'events': {
+        'mouseover': function () {
+          d3.select(this)
+            .style("stroke", 'red')
+            .style("stroke-width", 2);
+        },
+        'mouseout': function () {
+          d3.select(this)
+            .style("stroke", chart.config.radar.circle.stroke.color)
+            .style("stroke-width", chart.config.radar.circle.stroke.width);
+        }
+      }
+    })
+  };
 
   var chart = new dex.component(userConfig, defaults);
 
   chart.render = function render() {
     d3 = dex.charts.d3.d3v3;
-    window.onresize = this.resize;
-    chart.resize();
-  };
-
-  chart.resize = function resize() {
-    d3 = dex.charts.d3.d3v3;
-    if (chart.config.resizable) {
-      var width = d3.select(chart.config.parent).property("clientWidth");
-      var height = d3.select(chart.config.parent).property("clientHeight");
-      dex.console.log(chart.config.id + ": resize(" + width + "," + height + ")");
-      chart.attr("width", width).attr("height", height).update();
-    }
-    else {
-      chart.update();
-    }
+    chart.resize = this.resize(chart);
+    window.onresize = chart.resize;
+    return chart.resize();
   };
 
   chart.update = function () {
     d3 = dex.charts.d3.d3v3;
     var chart = this;
     var config = chart.config;
+    var margin = config.margin;
     var csv = config.csv;
 
-    d3.selectAll("#" + config.id).remove();
+    var width = config.width - margin.left - margin.right;
+    var height = config.height - margin.top - margin.bottom;
 
+    // Remove the old, build from scratch.
+    d3.selectAll(config.parent).selectAll('*').remove();
+
+    // REM: Port to dex.csv method generically
     data = csv.data.map(function (row) {
       return {
         'key': row[0],
@@ -5204,13 +5273,9 @@ var radarchart = function (userConfig) {
       }
     });
 
-    console.dir(data);
-
     data = data.map(function (d) {
       return d.values
-    })
-
-    console.dir(data);
+    });
 
     var extents = dex.matrix.extent(csv.data, dex.range(1, csv.header.length - 1));
 
@@ -5221,9 +5286,7 @@ var radarchart = function (userConfig) {
         return i.axis
       })),	//Names of each axis
       total = allAxis.length,					//The number of different axes
-      radius = Math.min(
-        (config.width - config.margin.left - config.margin.right) / 2,
-        (config.height - config.margin.top - config.margin.bottom) / 2),
+      radius = Math.min(width / 2, height / 2),
 
       //Format = d3.format('%'),			 	//Percentage formatting
       angleSlice = Math.PI * 2 / total;			//The width in radians of each "slice"
@@ -5233,25 +5296,33 @@ var radarchart = function (userConfig) {
       .range([0, radius])
       .domain([0, maxValue]);
 
+    // Store in config for chart calculations.
+    config.radiusScale = rScale;
+
     /////////////////////////////////////////////////////////
     //////////// Create the container SVG and g /////////////
     /////////////////////////////////////////////////////////
 
-    //Initiate the radar chart SVG
-    var chartContainer = d3.select(config.parent).append("g")
+    var chartContainer = d3.select(config.parent)
+      .append("g")
       .attr("id", config["id"])
       .attr("class", config["class"])
-      //.attr("width", config.width)
-      //.attr("height", config.height)
-      .attr("transform", config.transform +
-        "translate(" + config.width / 2 + " " + config.height / 2 + ")");
+      .attr('width', config.width)
+      .attr('height', config.height)
+      .attr("transform", config.transform);
+
+    var chartG = chartContainer
+      .append('g')
+      .attr('transform', 'translate(' +
+        margin.left + ',' + margin.top + ') translate(' +
+        width / 2 + ' ' + height / 2 + ")");
 
     /////////////////////////////////////////////////////////
     ////////// Glow filter for some extra pizzazz ///////////
     /////////////////////////////////////////////////////////
 
     //Filter for the outside glow
-    var filter = chartContainer.append('defs').append('filter').attr('id', 'glow'),
+    var filter = chartG.append('defs').append('filter').attr('id', 'glow'),
       feGaussianBlur = filter.append('feGaussianBlur').attr('stdDeviation', '2.5').attr('result', 'coloredBlur'),
       feMerge = filter.append('feMerge'),
       feMergeNode_1 = feMerge.append('feMergeNode').attr('in', 'coloredBlur'),
@@ -5262,7 +5333,7 @@ var radarchart = function (userConfig) {
     /////////////////////////////////////////////////////////
 
     //Wrapper for the grid & axes
-    var axisGrid = chartContainer.append("g").attr("class", "axisWrapper");
+    var axisGrid = chartG.append("g").attr("class", "axisWrapper");
 
     //Draw the background circles, broken in WebView
     axisGrid.selectAll(".levels")
@@ -5282,16 +5353,10 @@ var radarchart = function (userConfig) {
     axisGrid.selectAll(".axisLabel")
       .data(d3.range(1, (config.levels + 1)).reverse())
       .enter().append("text")
+      .call(dex.config.configureText, config.spoke.label)
       .attr("class", "axisLabel")
-      .attr("x", 4)
-      .attr("y", function (d) {
-        return -d * radius / config.levels;
-      })
-      .attr("dy", "0.4em")
-      .style("font-size", "10px")
-      .attr("fill", "#737373")
       .text(function (d, i) {
-        return maxValue * d / config.levels;
+        return config.format(maxValue * d / config.levels);
         //Format(maxValue * d / config.levels);
       });
 
@@ -5307,24 +5372,26 @@ var radarchart = function (userConfig) {
       .attr("class", "axis");
     //Append the lines
     axis.append("line")
+      .call(dex.config.configureLine, config.spoke.line)
       .attr("x1", 0)
       .attr("y1", 0)
       .attr("x2", function (d, i) {
-        return rScale(maxValue * 1.1) * Math.cos(angleSlice * i);
+        return rScale(maxValue * 1.05) * Math.cos(angleSlice * i);
       })
       .attr("y2", function (d, i) {
-        return rScale(maxValue * 1.1) * Math.sin(angleSlice * i);
+        return rScale(maxValue * 1.05) * Math.sin(angleSlice * i);
       })
-      .attr("class", "line")
-      .style("stroke", "white")
-      .style("stroke-width", "2px");
+      .attr("class", "line");
+//      .style("stroke", "white")
+//      .style("stroke-width", "2px");
 
     //Append the labels at each axis
     axis.append("text")
+      .call(dex.config.configureText, config.label)
       .attr("class", "legend")
-      .style("font-size", "11px")
-      .attr("text-anchor", "middle")
-      .attr("dy", "0.35em")
+      //.style("font-size", "11px")
+      //.attr("text-anchor", "middle")
+      //.attr("dy", "0.35em")
       .attr("x", function (d, i) {
         return rScale(maxValue * config.labelFactor) * Math.cos(angleSlice * i - Math.PI / 2);
       })
@@ -5333,8 +5400,8 @@ var radarchart = function (userConfig) {
       })
       .text(function (d) {
         return d
-      })
-      .call(wrap, config.wrapWidth);
+      });
+      //.call(wrap, config.wrapWidth);
 
     /////////////////////////////////////////////////////////
     ///////////// Draw the radar chart blobs ////////////////
@@ -5355,7 +5422,7 @@ var radarchart = function (userConfig) {
     }
 
     //Create a wrapper for the blobs
-    var blobWrapper = chartContainer.selectAll(".radarWrapper")
+    var blobWrapper = chartG.selectAll(".radarWrapper")
       .data(data)
       .enter().append("g")
       .attr("class", "radarWrapper");
@@ -5375,11 +5442,11 @@ var radarchart = function (userConfig) {
         //Dim all blobs
         d3.selectAll(".radarArea")
           .transition().duration(200)
-          .style("fill-opacity", 0.1);
+          .style("fill-opacity", 0.05);
         //Bring back the hovered over blob
         d3.select(this)
           .transition().duration(200)
-          .style("fill-opacity", 0.7);
+          .style("fill-opacity", .7);
       })
       .on('mouseout', function () {
         //Bring back all blobs
@@ -5425,7 +5492,7 @@ var radarchart = function (userConfig) {
     /////////////////////////////////////////////////////////
 
     //Wrapper for the invisible circles on top
-    var blobCircleWrapper = chartContainer.selectAll(".radarCircleWrapper")
+    var blobCircleWrapper = chartG.selectAll(".radarCircleWrapper")
       .data(data)
       .enter().append("g")
       .attr("class", "radarCircleWrapper");
@@ -5453,7 +5520,7 @@ var radarchart = function (userConfig) {
         tooltip
           .attr('x', newX)
           .attr('y', newY)
-          .text(Format(d.value))
+          .text(config.format(d.value))
           .transition().duration(200)
           .style('opacity', 1);
       })
@@ -5463,7 +5530,7 @@ var radarchart = function (userConfig) {
       });
 
     //Set up the small tooltip for when you hover over a circle
-    var tooltip = chartContainer.append("text")
+    var tooltip = chartG.append("text")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
@@ -5497,7 +5564,10 @@ var radarchart = function (userConfig) {
           }
         }
       });
-    }//wrap
+    }
+
+    // Allow method chaining
+    return chart;
   };
 
   $(document).ready(function () {
