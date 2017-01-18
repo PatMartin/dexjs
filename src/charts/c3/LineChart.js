09dccbbb03608c833ee6f3e224927dd90c1c469a
Patrick Martin <patrick145@msn.com>
2017-01-18 18:04:30 -0500

    Changed default linktype to line.

--- src/charts/c3/LineChart.js
+++ src/charts/c3/LineChart.js
@@ -40,7 +40,7 @@ var linechart = function (userConfig) {
             'header': [],
             'data': []
         },
-        'linktype' : 'area',
+        'linktype' : 'line',
         'width': "100%",
         'height': "100%"
     };
