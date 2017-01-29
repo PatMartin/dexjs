37dfda2085f6976fa3b85e38c04164aa4919be07
Patrick Martin <patrick145@msn.com>
2017-01-29 01:55:02 -0500

    Reworked Player to no longer require CSS and added label with frame number to it and a slider for sifting through the frames.

--- src/ui/jqueryui/Player.js
+++ src/ui/jqueryui/Player.js
@@ -11,8 +11,8 @@ var player = function (userConfig) {
     'delay': 1000,
     'frameIndex': 0,
     'csv': {
-      header : ['C1', 'C2', 'C3' ],
-      data : [
+      header: ['C1', 'C2', 'C3'],
+      data: [
         [1, 2, 3],
         [2, 3, 4],
         [3, 4, 5]
@@ -22,20 +22,66 @@ var player = function (userConfig) {
 
   var chart = new dex.component(userConfig, defaults);
   var config = chart.config;
-  var frames = dex.csv.getFramesByIndex(config.csv, config.frameIndex);
+  var frames;
   var frameNum = 0;
-  chart.attr("frames", frames);
 
   chart.render = function () {
     var timer;
     var state = "stopped";
     frames = dex.csv.getFramesByIndex(config.csv, config.frameIndex);
-    dex.console.log(frames);
     chart.attr("frames", frames);
 
-    dex.console.log("FRAMES:", frames);
+    //dex.console.log("FRAMES:", frames);
+
+    $(config.parent)
+      .append('<div><label>Frame:</label>' +
+        '<label id="frameNumber">1</label>')
+      .css('display', 'block')
+      .css('text-align', 'center');
+    $(config.parent)
+      .append("<div id='slider'></div>")
+      .css("width", (chart.config.width / 2) + "px")
+      .css("margin", "auto");
+    // Add our buttons.
+    $(config.parent)
+      .append("<div id='controls'></div>")
+      .css("display", "block")
+      .css("text-align", "center");
+    $("#controls")
+      .append("<button id='beginning'>go to beginning</button>");
+    $("#controls")
+      .append("<button id='previous'>previous</button>");
+    $("#controls")
+      .append("<button id='play'>play</button>");
+    $("#controls")
+      .append("<button id='next'>next</button>");
+    $("#controls")
+      .append("<button id='end'>go to end</button>");
 
     $(function () {
+
+      $("#slider").slider({
+        value: 1,
+        min: 1,
+        max: frames.frames.length,
+        step: 1,
+        classes: {
+          "ui-slider": "highlight"
+        },
+        'slide': function (event, ui) {
+          dex.console.log("VALUE", ui.value);
+          $("#frameNumber").html(ui.value)
+          gotoFrame(ui.value - 1);
+
+          // Stop playing
+          clearTimeout(timer);
+          $("#play").button({
+            "label": "play",
+            "icons": {"primary": "ui-icon-play"}
+          });
+        }
+      });
+
       $("#beginning").button({
         text: false,
         icons: {
@@ -53,11 +99,11 @@ var player = function (userConfig) {
         previous();
       });
       $("#play").button({
-          text: false,
-          icons: {
-            primary: "ui-icon-play"
-          }
-        })
+        text: false,
+        icons: {
+          primary: "ui-icon-play"
+        }
+      })
         .click(function () {
           var options;
           if ($(this).text() === "play") {
@@ -94,7 +140,7 @@ var player = function (userConfig) {
           primary: "ui-icon-seek-end"
         }
       }).click(function () {
-        gotoFrame(frames.frames.length-1);
+        gotoFrame(frames.frames.length - 1);
       });
     });
 
@@ -107,6 +153,8 @@ var player = function (userConfig) {
     }
 
     gotoFrame(0);
+
+    return chart;
   };
 
   chart.update = function () {
@@ -116,7 +164,7 @@ var player = function (userConfig) {
   };
 
   function previous() {
-    gotoFrame(frameNum > 0 ? (frameNum-1) : 0)
+    gotoFrame(frameNum > 0 ? (frameNum - 1) : 0)
   }
 
   function next() {
@@ -125,11 +173,16 @@ var player = function (userConfig) {
 
   function gotoFrame(frameIndex) {
     frameNum = frameIndex;
+    if ($("#slider").is(":ui-slider")) {
+      $("#slider").slider({ "value" : frameNum + 1 });
+      $("#frameNumber").html(frameNum + 1);
+    }
     chart.publish({
-      "type"  : "new-frame",
-      "data"  : frames.frames[frameNum],
-      "name"  : frames.frameIndices[frameNum],
-      "frameBy" : csv.header[config.frameIndex] }
+        "type": "new-frame",
+        "data": frames.frames[frameNum],
+        "name": frames.frameIndices[frameNum],
+        "frameBy": csv.header[config.frameIndex]
+      }
     );
     dex.console.log("Displaying frame: " + frameNum);
   }
