a47985755f5b682942e2d7a9a856d91436c87ba0
Patrick Martin <patrick145@msn.com>
2017-02-17 22:08:34 -0500

    Added permutations and combo routines

--- src/array/array.js
+++ src/array/array.js
@@ -79,6 +79,89 @@ module.exports = function array(dex) {
       return arraySlice;
     },
 
+    'getPermutations': function (list, maxLen) {
+      // Copy initial values as arrays
+      var perm = list.map(function (val) {
+        return [val];
+      });
+      // Our permutation generator
+      var generate = function (perm, maxLen, currLen) {
+        // Reached desired length
+        if (currLen === maxLen) {
+          return perm;
+        }
+        // For each existing permutation
+        for (var i = 0, len = perm.length; i < len; i++) {
+          var currPerm = perm.shift();
+          // Create new permutation
+          for (var k = 0; k < list.length; k++) {
+            perm.push(currPerm.concat(list[k]));
+          }
+        }
+        // Recurse
+        return generate(perm, maxLen, currLen + 1);
+      };
+      // Start with size 1 because of initial values
+      return generate(perm, maxLen, 1);
+    },
+
+    'getCombinations': function (list, comboLength) {
+      var i, j, combs, head, tailcombs;
+
+      // There is no way to take e.g. sets of 5 elements from
+      // a set of 4.
+      if (comboLength > list.length || comboLength <= 0) {
+        return [];
+      }
+
+      // K-sized set has only one K-sized subset.
+      if (comboLength == list.length) {
+        return [list];
+      }
+
+      // There is N 1-sized subsets in a N-sized set.
+      if (comboLength == 1) {
+        combs = [];
+        for (i = 0; i < list.length; i++) {
+          combs.push([list[i]]);
+        }
+        return combs;
+      }
+
+      // Assert {1 < k < set.length}
+
+      // Algorithm description:
+      // To get k-combinations of a set, we want to join each element
+      // with all (k-1)-combinations of the other elements. The set of
+      // these k-sized sets would be the desired result. However, as we
+      // represent sets with lists, we need to take duplicates into
+      // account. To avoid producing duplicates and also unnecessary
+      // computing, we use the following approach: each element i
+      // divides the list into three: the preceding elements, the
+      // current element i, and the subsequent elements. For the first
+      // element, the list of preceding elements is empty. For element i,
+      // we compute the (k-1)-computations of the subsequent elements,
+      // join each with the element i, and store the joined to the set of
+      // computed k-combinations. We do not need to take the preceding
+      // elements into account, because they have already been the i:th
+      // element so they are already computed and stored. When the length
+      // of the subsequent list drops below (k-1), we cannot find any
+      // (k-1)-combs, hence the upper limit for the iteration:
+      combs = [];
+      for (i = 0; i < list.length - comboLength + 1; i++) {
+        // head is a list that includes only our current element.
+        head = list.slice(i, i + 1);
+        // We take smaller combinations from the subsequent elements
+        tailcombs = dex.array.getCombinations(list.slice(i + 1), comboLength - 1);
+        // For each (k-1)-combination we join it with the current
+        // and store it to the set of k-combinations.
+        for (j = 0; j < tailcombs.length; j++) {
+          combs.push(head.concat(tailcombs[j]));
+        }
+      }
+      return combs;
+    },
+
     /**
      *
      * This method locates the array element whose id tag matches the supplied
@@ -214,7 +297,7 @@ module.exports = function array(dex) {
       // Deep copy:
       //return $.extend(true, {}, array);
     },
-    'isNumeric' : function(array) {
+    'isNumeric': function (array) {
       return array.every(dex.object.isNumeric);
     }
   };
