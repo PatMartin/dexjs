89bbb0ca8608d217fc5f995cc2d30fc8eb448e62
Patrick Martin <patrick145@msn.com>
2017-04-29 18:13:54 -0400

    multiples support

--- src/charts/charts.js
+++ src/charts/charts.js
@@ -17,13 +17,14 @@
  */
 module.exports = function charts() {
   return {
-    'c3'      : require("./c3/c3"),
-    'd3'      : require("./d3/d3"),
-    'd3plus'  : require("./d3plus/d3plus"),
-    'echarts' : require("./echarts/echarts"),
-    'elegans' : require("./elegans/elegans"),
-    'nvd3'    : require("./nvd3/nvd3"),
-    'threejs' : require("./threejs/threejs"),
-    'vis'     : require("./vis/vis")
+    'c3'       : require("./c3/c3"),
+    'd3'       : require("./d3/d3"),
+    'd3plus'   : require("./d3plus/d3plus"),
+    'echarts'  : require("./echarts/echarts"),
+    'elegans'  : require("./elegans/elegans"),
+    'multiples' : require("./multiples/multiples"),
+    'nvd3'     : require("./nvd3/nvd3"),
+    'threejs'  : require("./threejs/threejs"),
+    'vis'      : require("./vis/vis")
   };
 };
