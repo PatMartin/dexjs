887fbd81e991d9e24830fd9ebccc48b2a06134df
PatMartin <patrick145@msn.com>
2019-02-23 15:52:06 -0500

    New grid visual

---
+++ src/charts/grid/grid.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * This module contains components related to grids and tables of information.
+ *
+ * @module dex/charts/grid
+ *
+ */
+var grid = {};
+
+grid.JqGrid = require("./JqGrid");
+
+module.exports = grid;
