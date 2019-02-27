d5c0bf47d96bad7bd5e6148ff99eddd8b25e000e
PatMartin <patrick145@msn.com>
2019-02-26 20:15:20 -0500

    Added bluebird promises.

--- src/dex-libs.js
+++ src/dex-libs.js
@@ -1,8 +1,8 @@
 var dexlibs = window.dexbootstrap || window.dexjquery || {};
 
 // Dependencies:
-// https://github.com/federico-lox/pubsub.js
+
 dexlibs.bus = require("../lib/pubsub");
 dexlibs.moment = require("../lib/moment/moment");
-
+dexlibs.promise = require("../lib/bluebird/bluebird");
 module.exports = dexlibs;
