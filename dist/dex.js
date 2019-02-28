7c504e1c3ff9e1cf0d7746a00da90b66685c7314
PatMartin <patrick145@msn.com>
2019-02-28 01:44:36 -0500

    Added transofrmers and changed io module a bit

--- dist/dex.js
+++ dist/dex.js
@@ -24972,7 +24972,7 @@ module.exports = function (dex) {
     });
   };
 
-  io.readXml = function (path, xpaths) {
+  io.readXmlOld = function (path, xpaths) {
     return new dex.promise(function (resolve) {
 
       var header = Object.keys(xpaths);
@@ -25009,13 +25009,83 @@ module.exports = function (dex) {
     });
   };
 
+  io.transform = {
+    xml: {}
+  };
+
+  io.transform.xml.xpath = function (xpaths) {
+    return function (xml) {
+      var header = Object.keys(xpaths);
+      var csv = new dex.csv(header);
+
+      header.map(function (h) {
+        //dex.console.log("XPATHS[" + h + "] = " + xpaths[h]);
+        xpath = xml.evaluate(xpaths[h], xml);
+        //dex.console.log(xpath);
+        var xi = 0;
+        try {
+          var thisNode = xpath.iterateNext();
+
+          while (thisNode) {
+            if (!csv.data[xi]) {
+              csv.data[xi] = [thisNode.textContent];
+            }
+            else {
+              csv.data[xi].push(thisNode.textContent);
+            }
+            xi++;
+            //dex.console.log("NODE", thisNode.textContent);
+            thisNode = xpath.iterateNext();
+          }
+        }
+        catch (e) {
+          alert('Error: Document tree modified during iteration ' + e);
+        }
+      });
+      return csv;
+    };
+  };
+
+  io.transform.xml.rows = function (xml) {
+    var xpath = "//row[1]/*";
+    var xpaths = {};
+    xpath = xml.evaluate(xpath, xml);
+
+    try {
+      var thisNode = xpath.iterateNext();
+
+      while (thisNode) {
+        //dex.console.log("NODE", thisNode.localName);
+        xpaths[thisNode.localName] = "//row/" + thisNode.localName;
+        thisNode = xpath.iterateNext();
+      }
+    }
+    catch (e) {
+      alert('Error: Document tree modified during iteration ' + e);
+    }
+
+    //dex.console.log("XPATHS", xpaths);
+    return io.transform.xml.xpath(xpaths)(xml);
+  };
+
+  io.readXml = function (path, transformer) {
+    var transform = transformer || io.transform.xml.rows;
+
+    return new dex.promise(function (resolve) {
+
+      d3.xml(path, function (xml) {
+        resolve(transform(xml));
+      });
+    });
+  };
+
   io.readJson = function (path, transformer) {
-    var transform = transformer || function(json) { return json; }
+    var transform = transformer || function(json) { return new dex.csv(json); }
 
     return new dex.promise(function (resolve) {
 
       d3.json(path, function (json) {
-        resolve(new dex.csv(transform(json)));
+        resolve(transform(json));
       });
     });
   };
